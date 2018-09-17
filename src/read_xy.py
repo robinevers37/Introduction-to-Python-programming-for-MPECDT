8c96a51874fdc08ff6922d8020a1edb2d4cd3beb
Robin Evers <r.evers18@imperial.ac.uk>
2018-09-17 14:18:52 +0100

    read_xy.py

---
+++ src/read_xy.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pylab as pl
+
+with open("../data/xy.dat", "r") as infile:
+    x = []
+    y = []
+    for line in infile:
+        x_, y_ = [float(w) for w in line.split()]
+        x.append(x_)
+        y.append(y_)
+
+x = np.array(x)
+y = np.array(y)
+
+pl.plot(x,y)
+pl.show()
